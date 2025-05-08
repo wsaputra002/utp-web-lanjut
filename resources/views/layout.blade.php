3ca0a05e8648017f5d9b6cb4c8e022036198564c
wsaputra002 <wayusuryasaputra920@gmail.com>
2025-05-08 23:52:34 +0700

    Part 2

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,72 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Student Management System</title>
+
+    <style>
+        .sidebar {
+            margin: 0;
+            padding: 0;
+            width: 200px;
+            background-color: #f1f1f1;
+            position: fixed;
+            height: 100%;
+            overflow: auto;
+        }
+
+        /* Sidebar links */
+        .sidebar a {
+            display: block;
+            color: black;
+            padding: 16px;
+            text-decoration: none;
+        }
+
+        /* Active/current link */
+        .sidebar a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+
+        /* Links on mouse-over */
+        .sidebar a:hover:not(.active) {
+            background-color: #555;
+            color: white;
+        }
+
+        /* Page content. The value of the margin-left property should match the value of the sidebar's width property */
+        div.content {
+            margin-left: 200px;
+            padding: 1px 16px;
+            height: 1000px;
+        }
+
+        /* On screens that are less than 700px wide, make the sidebar into a topbar */
+        @media screen and (max-width: 700px) {
+            .sidebar {
+                width: 100%;
+                height: auto;
+                position: relative;
+            }
+
+            .sidebar a {
+                float: left;
+            }
+
+            div.content {
+                margin-left: 0;
+            }
+        }
+
+        /* On screens that are less than 400px, display the bar vertically, instead of horizontally */
+        @media screen and (max-width: 400px) {
+            .sidebar a {
+                text-align: center;
+                float: none;
+            }
+        }
+    </style>
+
 </head>
 
 <body>
@@ -25,10 +90,36 @@
                         aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-
                 </nav>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <!-- The sidebar -->
+                <div class="sidebar">
+                    <a class="active" href="#home">Home</a>
+                    <a href="#news">Student</a>
+                    <a href="#contact">Teacher</a>
+                    <a href="#contact">Courses</a>
+                    <a href="#about">Enrollment</a>
+                    <a href="#about">Payment</a>
+                </div>
+            </div>
+            <div class="col-md-9">
+
+                <div class="content">
+                    @yield('content')
+
+                </div>
+
+            </div>
+
+
+
+        </div>
+    </div>
+
 
 </body>
 
