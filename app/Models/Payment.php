fec5c2ea116efbea9b8eed74d1d31741abb0c12c
wsaputra002 <wayusuryasaputra920@gmail.com>
2025-05-12 19:43:43 +0700

    Part 11

---
+++ app/Models/Payment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+    protected $table = 'enrollments';
+    protected $primaryKey = 'id';
+    protected $fillable = ['enrollment_id', 'paid_date', 'amount'];
+
+    public function enrollment(){
+        return $this->belongsTo(Enrollment::class);
+    }
+}
---
+++ resources/views/payments/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+@section('content')
+
+    <div class="card">
+        <div class="card-header">Batches</div>
+        <div class="card-body">
+
+            <form action="{{ url('batches') }}" method="post">
+                @csrf
+
+                <label>Enroll No</label><br>
+                <select name="enrollment_id" id="enrollment_id" class="form-control">
+                    @foreach($payments as $id => $enroll_no)
+                        <option value="{{ $id }}">{{ $enroll_no }}</option>
+                    @endforeach
+                </select><br>
+
+                <label>Paid Date</label><br>
+                <input type="text" name="paid_date" id="paid_date" class="form-control"><br>
+                <label>Amount</label><br>
+                <input type="text" name="amount" id="amount" class="form-control"><br>
+
+                <input type="submit" value="Save" class="btn btn-success">
+            </form>
+
+        </div>
+    </div>
+
+@endsection
